b6aa8ec793258b14ed0804759cac57dc13be9542
Oleg Koptev <olegstankoptev@gmail.com>
2020-11-08 20:48:32 +0300

    Initial Commit

---
+++ task3/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  task3
+//
+//  Created by Oleg Koptev on 08.11.2020.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
