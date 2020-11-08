3a99ea247700e7f177bcb6ee10cd33ea065f9f90
Oleg Koptev <olegstankoptev@gmail.com>
2020-11-08 22:19:07 +0300

    Single threaded execution finished

--- task3/main.cpp
+++ task3/main.cpp
@@ -8,10 +8,14 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <sstream>
 using namespace std;
 
 #define NUM_THREADS 4
 
+/**
+ * Definition of 3 space lengths
+ */
 class Point3D {
 private:
     float x_;
@@ -25,20 +29,54 @@ public:
     float z() { return z_; }
 };
 
+/**
+ * Structure containing 3 vectors and boolean value it they are coplanar
+ */
 struct Triplet {
     Point3D triplet[3];
     bool bCoplanar;
 };
 
+typedef struct {
+    int start_index;
+    int end_index;
+    vector<Triplet> triplets;
+} pthrData;
+
+void* threadFunc(void* thread_data) {
+    pthrData *data = (pthrData*) thread_data;
+    auto start = next(data->triplets.begin(), data->start_index);
+    auto end = next(data->triplets.begin(), data->end_index);
+    
+    for (auto i = start; i != end; ++i) {
+        Triplet* triplet = &(*i);
+        float value =
+            triplet->triplet[0].x() *
+                (triplet->triplet[1].y() * triplet->triplet[2].z() -
+                 triplet->triplet[1].z() * triplet->triplet[2].y()) +
+            triplet->triplet[0].y() *
+                (triplet->triplet[1].z() * triplet->triplet[2].x() -
+                 triplet->triplet[1].x() * triplet->triplet[2].z()) +
+            triplet->triplet[0].z() *
+                (triplet->triplet[1].x() * triplet->triplet[2].y() -
+                 triplet->triplet[1].y() * triplet->triplet[2].x());
+        if (value == 0) {
+            triplet->bCoplanar = true;
+        }
+    }
+    
+    return NULL;
+};
+
 int main(int argc, const char * argv[]) {
     
-    // 1. Check for correct amount of arguments
+//  1. Check for correct amount of arguments
     if (argc != 3) {
         cout << "Wrong amount of arguments\n";
         return 1;
     }
     
-    // 2. Read all vectors from input file
+//  2. Read all vectors from input file
     vector<Point3D> vectors;
     float x, y, z;
     ifstream input (argv[1]);
@@ -53,10 +91,11 @@ int main(int argc, const char * argv[]) {
     }
     
     
-    // 3. Form an array of all possible triplets
+//  3. Form an array of all possible triplets
     long amountOfPoints = vectors.size();
     long amountOfTriplets = amountOfPoints * (amountOfPoints - 1) * (amountOfPoints - 2) / 6;
     vector<Triplet> triplets(amountOfTriplets);
+    int tripletIndex = 0;
     for (auto i = vectors.begin(); i != vectors.end(); ++i) {
         for (auto j = next(i, 1); j != vectors.end(); ++j) {
             for (auto k = next(j, 1); k != vectors.end(); ++k) {
@@ -65,11 +104,47 @@ int main(int argc, const char * argv[]) {
                 triplet.triplet[1] = *j;
                 triplet.triplet[2] = *k;
                 triplet.bCoplanar = false;
+                
+                triplets[tripletIndex] = triplet;
+                tripletIndex++;
             }
         }
     }
     
+//  4. Calculate coplanar property for all triplets
+    for (auto i = triplets.begin(); i != triplets.end(); ++i) {
+        Triplet* triplet = &(*i);
+        float value =
+            triplet->triplet[0].x() *
+                (triplet->triplet[1].y() * triplet->triplet[2].z() -
+                 triplet->triplet[1].z() * triplet->triplet[2].y()) +
+            triplet->triplet[0].y() *
+                (triplet->triplet[1].z() * triplet->triplet[2].x() -
+                 triplet->triplet[1].x() * triplet->triplet[2].z()) +
+            triplet->triplet[0].z() *
+                (triplet->triplet[1].x() * triplet->triplet[2].y() -
+                 triplet->triplet[1].y() * triplet->triplet[2].x());
+        if (value == 0) {
+            triplet->bCoplanar = true;
+        }
+    }
     
+//  5. Write coplanar triplets
+    stringstream outputString;
+    for (auto i = triplets.begin(); i != triplets.end(); ++i) {
+        Triplet triplet = *i;
+        if (!triplet.bCoplanar) { continue; }
+        outputString << "(("
+            << triplet.triplet[0].x() << ", " << triplet.triplet[0].y() << ", " << triplet.triplet[0].z() << "), ("
+            << triplet.triplet[1].x() << ", " << triplet.triplet[1].y() << ", " << triplet.triplet[1].z() << "), ("
+            << triplet.triplet[2].x() << ", " << triplet.triplet[2].y() << ", " << triplet.triplet[2].z() << "))\n";
+    }
+    ofstream output (argv[2]);
+    if (output.is_open()) {
+        output << outputString.str();
+    } else {
+        cout << "Error when writing to output file\n";
+    }
     
     
     return 0;
